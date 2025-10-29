147ba8d964eee1a638f443f90751512a38691767
ElSantanax <josesantanaserrano130598@gmail.com>
2025-10-29 13:26:15 -0500

    Update getCrytos function to validate response data using CrytoCurrenciesResponseShema; add schema definition for cryptocurrency response

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,12 @@
 import { create } from 'zustand'
 import axios from 'axios';
+import { CrytoCurrenciesResponseShema } from './schema/crypto-shema';
 
 async function getCrytos() {
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
     const { data: { Data } } = await axios(url)
-    console.log(Data);
+    const result = CrytoCurrenciesResponseShema.safeParse(Data)
+    console.log(result);
 }
 
 export const useCryptoStore = create(() => ({

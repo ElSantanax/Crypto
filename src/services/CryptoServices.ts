3496f3713ffa9312d3c50868f4dcaffc5715d333
ElSantanax <josesantanaserrano130598@gmail.com>
2025-10-31 10:19:28 -0500

    Enhance fetchCurrentCryptoPrice function to validate and return structured cryptocurrency price data using CryptoPriceSchema; update store to log fetched results for better debugging.

--- src/services/CryptoServices.ts
+++ src/services/CryptoServices.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { CryptoCurrenciesResponseShema } from '../schema/crypto-shema';
+import { CryptoCurrenciesResponseShema, CryptoPriceSchema } from '../schema/crypto-shema';
 import type { Pair } from '../types';
 
 export async function getCrytos() {
@@ -15,5 +15,9 @@ export async function getCrytos() {
 export async function fetchCurrentCryptoPrice(pair: Pair) {
     const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${pair.criptocurrency}&tsyms=${pair.currency}`
     const { data: { DISPLAY } } = await axios(url)
-    console.log(DISPLAY[pair.criptocurrency], [pair.currency]);
+    const result = CryptoPriceSchema.safeParse(DISPLAY[pair.criptocurrency][pair.currency])
+
+    if (result.success) {
+        return result.data;
+    }
 }
